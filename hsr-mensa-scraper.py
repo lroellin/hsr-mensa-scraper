54bee85896f3339f1ff189e235b046bf082d7144
Lukas Röllin <lsroellin@gmail.com>
2017-05-10 10:36:44 +0200

    Initial commit

--- hsr-mensa-scraper.py
+++ hsr-mensa-scraper.py
@@ -2,10 +2,16 @@ import logging
 import requests
 from bs4 import BeautifulSoup
 
+
 class Sites:
     def __init__(self, url, name):
         self.url = url
         self.name = name
+
+class MenuItem:
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
 
 def main():
     logging.basicConfig(level=logging.INFO)
@@ -17,7 +23,13 @@ def main():
     for site in sites:
         logging.info("Loading site " + site.url)
         request = requests.get(site.url)
+        data = request.text
+        soup = BeautifulSoup(data, "html.parser")
 
+        todayElements = soup.find('div', {'id': 'menu-plan-tab1'})
+        for singleElement in todayElements.findAll('div', {'class': 'menu-item'}):
+            print(singleElement.find('h2', {'class': 'menu-title'}).contents[0])
+            print(singleElement.find('p', {'class': 'menu-description'}).contents[0])
 
 
 if __name__ == "__main__":
