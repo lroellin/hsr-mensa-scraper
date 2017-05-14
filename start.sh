cf5006658344ad0106b4b1603cb149f1dcbc6573
Lukas Röllin <lsroellin@gmail.com>
2017-05-14 21:37:10 +0200

    Remove old nohup file

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ set -x
 
 killall -9 uwsgi || true
 git pull
+rm nohup.out
 nohup uwsgi -s /srv/http/hsr-mensa-scraper/uwsgi.sock --chmod-socket=666 --manage-script-name --mount /=rest:application --plugin python3 &
 service nginx restart
