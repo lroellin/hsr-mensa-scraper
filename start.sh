e5bfa00871cfa3f6d7550234c77f80ba18bee9ef
Lukas Röllin <lsroellin@gmail.com>
2017-05-14 21:35:19 +0200

    Changed startup script

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-git pull
 killall -9 uwsgi || true
+git pull
 nohup uwsgi -s /srv/http/hsr-mensa-scraper/uwsgi.sock --chmod-socket=666 --manage-script-name --mount /=rest:application --plugin python3 &
 service nginx restart
